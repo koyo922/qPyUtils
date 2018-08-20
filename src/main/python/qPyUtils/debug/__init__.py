f59568b6d50a48379d246b763617cae7a36c95a4
QIAN Weishuo <koyo922@qq.com>
2018-08-20 23:02:49 +0800

    ADD: auto_unstub() into debug

---
+++ src/main/python/qPyUtils/debug/__init__.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim: tabstop=4 shiftwidth=4 expandtab number
+"""
+utils for debug
+
+Authors: qianweishuo<qzy922@gmail.com>
+Date:    2018/8/20 下午10:21
+"""
+from unittest import TestCase
+
+from mockito import unstub
+
+
+def auto_unstub(test_suite):
+    # type: (TestCase) -> TestCase
+    """
+    decorator for TestCase, adding ability to auto unstub mockito
+    thanks: https://gist.github.com/jnape/5767029
+    :param test_suite:
+    :return:
+    """
+    wrapped_tearDown = test_suite.tearDown if 'tearDown' in dir(test_suite) else lambda self: True
+
+    def tearDown(self):
+        try:
+            wrapped_tearDown(self)
+        except:
+            raise
+        finally:
+            unstub()
+
+    test_suite.tearDown = tearDown
+    return test_suite
