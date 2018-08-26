358af055be19886abe6a9c5224617dbfc4d133e0
QIAN Weishuo <koyo922@qq.com>
2018-08-27 00:45:27 +0800

    ADD: constant.true_fn()

--- src/unittest/python/constant_tests.py
+++ src/unittest/python/constant_tests.py
@@ -4,9 +4,19 @@
 import sys
 from unittest import TestCase
 
-from qPyUtils.constant import INF
+from qPyUtils.constant import INF, identity_fn, true_fn
 
 
 class TestConstant(TestCase):
     def test_inf(self):
         self.assertEqual(sys.maxsize, INF)
+
+    def test_identity(self):
+        obj = dict(key='val')
+        self.assertEqual(obj, identity_fn(obj))
+
+    def test_true(self):
+        self.assertTrue(true_fn([]))
+        self.assertTrue(true_fn({}))
+        self.assertTrue(true_fn(None))
+        self.assertTrue(true_fn())
